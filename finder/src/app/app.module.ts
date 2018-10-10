b1ab909c4803e883f7031c702c123fad4104e78b
Amit Singh <amit.singh1@zensar.com>
2018-10-10 11:47:22 +0530

    add component

--- finder/src/app/app.module.ts
+++ finder/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { DatePipe } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
+import { AdvancedSearchComponent } from './advanced-search/advanced-search.component';
+import { AuctionSearchComponent } from './auction-search/auction-search.component';
+import { environment} from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdvancedSearchComponent,
+    AuctionSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- finder/src/environments/environment.ts
+++ finder/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :  {
+    apiKey: "AIzaSyDk3oKx6iUHE7ZmAlDUMVfuUZN0mv8Mheo",
+    authDomain: "amcrud27.firebaseapp.com",
+    databaseURL: "https://amcrud27.firebaseio.com",
+    projectId: "amcrud27",
+    storageBucket: "amcrud27.appspot.com",
+    messagingSenderId: "37605649746"
+  }
 };
 
 /*
